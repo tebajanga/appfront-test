8d2a90143322a71642bf00446829f94f43fa2242
Timothy <anthony.timothy90@gmail.com>
2026-05-17 04:45:19 +0300

    refactor: adding exchange rate service and modifying product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Services\ExchangeRateService;
 
 class ProductController extends Controller
 {
+    public function __construct(
+        private ExchangeRateService $exchangeRateService
+    ) {}
+
     public function index()
     {
         $products = Product::all();
-        $exchangeRate = $this->getExchangeRate();
+        $exchangeRate = $this->exchangeRateService->getUsdToEurRate();
 
         return view('products.list', compact('products', 'exchangeRate'));
     }
@@ -19,42 +24,8 @@ class ProductController extends Controller
     {
         $id = $request->route('product_id');
         $product = Product::find($id);
-        $exchangeRate = $this->getExchangeRate();
+        $exchangeRate = $this->exchangeRateService->getUsdToEurRate();
 
         return view('products.show', compact('product', 'exchangeRate'));
-    }
-
-    /**
-     * @return float
-     */
-    private function getExchangeRate()
-    {
-        try {
-            $curl = curl_init();
-
-            curl_setopt_array($curl, [
-                CURLOPT_URL => "https://open.er-api.com/v6/latest/USD",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_TIMEOUT => 5,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-            ]);
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            if (!$err) {
-                $data = json_decode($response, true);
-                if (isset($data['rates']['EUR'])) {
-                    return $data['rates']['EUR'];
-                }
-            }
-        } catch (\Exception $e) {
-
-        }
-
-        return config('services.currency.exchange_rate');
     }
 }
---
+++ app/Services/ExchangeRateService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class ExchangeRateService
+{
+    public function getUsdToEurRate(): float
+    {
+        return Cache::remember('exchange_rate_usd_eur', now()->addMinutes(30), function () {
+            try {
+                $response = Http::timeout(10)
+                    ->get('https://open.er-api.com/v6/latest/USD');
+
+                if (! $response->successful()) {
+                    Log::warning('Exchange rate API returned unsuccessful response', [
+                        'status' => $response->status(),
+                    ]);
+
+                    return $this->fallbackRate();
+                }
+
+                return (float) data_get($response->json(), 'rates.EUR', $this->fallbackRate());
+            } catch (\Throwable $exception) {
+                Log::error('Exchange rate API request failed', [
+                    'error' => $exception->getMessage(),
+                ]);
+
+                return $this->fallbackRate();
+            }
+        });
+    }
+
+    private function fallbackRate(): float
+    {
+        return (float) config('services.currency.exchange_rate', 0.85);
+    }
+}
